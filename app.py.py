d226ac0232bc4378455957f423235537551c5062
acanas027 <acanas02@ku.edu>
2026-06-03 13:15:41 -0500

    Update app.py.py

--- app.py.py
+++ app.py.py
@@ -1699,7 +1699,7 @@ Example: "Behind 5 loads: should have 17 done by now, have done 12. Target have 
             model="openai/gpt-oss-120b",
             messages=[{"role": "user", "content": prompt}],
             temperature=0.2,
-            max_completion_tokens=3267,
+            max_completion_tokens=3260,
             extra_body={"include_reasoning" : False},
         )
         return response.choices[0].message.content
